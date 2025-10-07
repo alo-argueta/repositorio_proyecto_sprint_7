b22f58929c19505fc967c7b9f06bf1a021fa8ed3
Alondra <arguetaalondra716@gmail.com>
2025-10-06 19:27:25 -0600

    gráfico de dispersión

--- app.py
+++ app.py
@@ -15,3 +15,18 @@ if hist_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = st.button('Construir gráfico de dispersión')  # crear un botón
+
+if scatter_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un gráfico de dispersión
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    fig.show()  # crear gráfico de dispersión
